f132e6c1d66e26605d3cf93607b04e52b245a2c8
manjerokki <manjerokki@gmail.com>
2015-06-18 11:30:53 +0300

    add layout

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express')
+  , ejsLocals = require('ejs-locals')
   , app = express()
   , pages = require(__dirname + '/controllers/pages')
 
 // configuration settings 
+app.engine('ejs', ejsLocals)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
@@ -10,8 +12,6 @@ app.use(express.static(__dirname + '/public'))
 // mount routes
 app.get('/', function (req, res) { res.redirect('/home') })
 app.get('/home', pages.home)
-
-// app.get('/about', function (req, res) { res.redirect('/about') })
 app.get('/about', pages.about)
 
 module.exports = app
